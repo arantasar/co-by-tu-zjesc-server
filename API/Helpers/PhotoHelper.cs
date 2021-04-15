8dfa68a3787ca0b79ac3d64ec048b72cc98efa40
Janusz Guzowski <janusz.guzowski@gmail.com>
2021-04-15 20:24:43 +0200

    Improve Docker settings

--- API/Helpers/PhotoHelper.cs
+++ API/Helpers/PhotoHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Persistence.Models;
 using System;
 using System.IO;
@@ -9,23 +10,26 @@ namespace API.Helpers
 {
     public class PhotoHelper
     {
-        public PhotoHelper(IWebHostEnvironment webHostEnvironment)
+        public PhotoHelper(IWebHostEnvironment webHostEnvironment, IConfiguration configuration)
         {
             WebHostEnvironment = webHostEnvironment;
+            Configuration = configuration;
+            
         }
 
         public IWebHostEnvironment WebHostEnvironment { get; }
+        private readonly IConfiguration Configuration;
 
         public string AddPhoto(IFormFile photo, HttpContext httpContext, string directoryNameLev1, string directoryNameLev2 = null)
         {
             string uploadsFolder = Path.Combine(WebHostEnvironment.WebRootPath, directoryNameLev1, directoryNameLev2);
             string uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
             string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-            string prefix = httpContext.Request.IsHttps ? "https://" : "http://";
+            string prefix = Configuration["Host"] == "cobytuzjesc.pl" ? "https://" : "http://";
 
             photo.CopyTo(new FileStream(filePath, FileMode.Create));
             
-            uniqueFileName = prefix + httpContext.Request.Host.Value.ToString() + "/" + directoryNameLev1 + "/" + directoryNameLev2 + "/" + uniqueFileName;
+            uniqueFileName = prefix + Configuration["Host"] + "/" + directoryNameLev1 + "/" + directoryNameLev2 + "/" + uniqueFileName;
 
             return uniqueFileName;
         }
